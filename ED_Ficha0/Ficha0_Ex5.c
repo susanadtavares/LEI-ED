6f10ddc7756850f39c479b2b2d007178fe2255e1
Susana Tavares <susana15tavares@hotmail.com>
2025-12-10 22:11:21 +0000

    feat: worksheet 0 ex 5

---
+++ ED_Ficha0/Ficha0_Ex5.c
@@ -0,0 +1,12 @@
+
+int main() {
+
+    // Aqui só um exemplo. Depois completas com os teus testes.
+
+    int M = 3, N = 3;
+    int mat[] = {0,0,0, 1,0,0, 0,0,0};
+
+    printf("Esparsa? %d\n", Esparsa(mat, M, N, 70));
+
+    return 0;
+}
